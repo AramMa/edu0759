61ca1c75cac00334a1c639bd0be73762f0874dfc
AramMa <dth_soda@mail.ru>
2021-06-30 20:32:31 +0300

    проверка

---
+++ Quiz/app/src/main/java/com/example/quiz/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.quiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private Button yesBtn;
+    private Button noBtn;
+    private TextView questionTextView;
+
+    private Question[] questions = {
+            new Question(R.string.question, true),
+            new Question(R.string.question1, true),
+            new Question(R.string.question2, false),
+            new Question(R.string.question3, false),
+            new Question(R.string.question4, true),
+    };
+    private int questionIndex = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState != null) {
+            questionIndex = savedInstanceState.getInt("question", 0);
+        }
+
+        questionTextView = findViewById(R.id.questionTextView);
+        questionTextView.setText(questions[0].getQuestion());
+        yesBtn = findViewById(R.id.yesBtn);
+        noBtn = findViewById(R.id.noBtn);
+        yesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(questions[questionIndex].isAnswer()){
+                    Toast.makeText(MainActivity.this, R.string.correct, Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this, R.string.incorrect, Toast.LENGTH_SHORT).show();
+                }
+                questionIndex = (questionIndex + 1)% questions.length;
+                questionTextView.setText(questions[questionIndex].getQuestion());
+
+            }
+        });
+        noBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!questions[questionIndex].isAnswer()){
+                    Toast.makeText(MainActivity.this, R.string.correct, Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this, R.string.incorrect, Toast.LENGTH_SHORT).show();
+                }
+                questionIndex = (questionIndex + 1)% questions.length;
+                questionTextView.setText(questions[questionIndex].getQuestion());
+            }
+        });
+
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt("question", questionIndex);
+    }
+}
